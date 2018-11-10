10ba196fcda9ccb4b57add0f75aa2f2bd0437326
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-10 15:14:19 +0000

    Added lifecycle hook to fetch todos. Ecountered CORS related issue.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,19 @@
 import React, {Component} from 'react';
 
 class Todo extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            todo: []
+        }
+    }
+
+    componentWillMount(){
+        fetch('http://localhost:3000/api/todos')
+        .then(res => res.json())
+        .then(todos => this.setState({todos}))
+    }
+    
     render(){
         return (
             <h1>Todo</h1>
