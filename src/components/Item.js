55c923488cfca1ad86f0c10972c401a2423c6286
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-11 13:24:50 +0000

    Added line through on Item if todo is completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const Item = (props) => (
-    <li
-        onClick={() => props.onClick(props.completed, props._id)}>
+    <li>
+        <span
+        onClick={() => props.onClick(props.completed, props._id)}
+        style={{textDecoration: props.completed ? 'line-through' : 'none'}}>
         {props.name}
+        </span>
     </li>
 );
 
